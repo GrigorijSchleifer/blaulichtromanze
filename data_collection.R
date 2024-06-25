4d23e886aa516a18fa98e2f5c7941e7534e56dc2
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2024-06-25 05:47:24 +0200

    guess_encoding(file) did it

--- data_collection.R
+++ data_collection.R
@@ -3,18 +3,22 @@ require(readr)
 data_folder <- "~/github/blaulichtromanze/Krankenhausdaten"
 file_list <- list.files(path = data_folder, pattern = "*.csv") 
 
-# read in each .csv file in file_list and create a data frame with the same name as the .csv file
-for (i in 1:length(file_list)) {
-    assign(file_list[i],
-    read_delim(file = paste(data_folder, file_list[i], sep = "/"),
-               delim = ";", 
-               escape_double = FALSE, 
-               trim_ws = TRUE)
-    )
+read_kh_data <- function(file_location) {
+    # read in each .csv file in file_list
+    # create a data frame with the same name as the .csv file
+    # Args:
+        # character vector with locating the files
+    # Return:
+        # not sure yet
+    for (i in 1:length(file_list)) {
+        assign(file_list[i],
+               read_delim(file = paste(data_folder, file_list[i], sep = "/"),
+                          delim = ";",
+                          escape_double = FALSE, 
+                          trim_ws = TRUE,
+                          locale = locale(encoding = "ISO-8859-1"))
+        )
+    }
 }
 
-nrw_khs <- read_delim("Krankenhausdaten/krankenhaeuser_EPSG25832_CSV.csv", 
-                                           delim = ";", escape_double = FALSE, trim_ws = TRUE)
-nrw_khs |> 
-    dplyr::filter(bs_gemeinde == "Bonn") |> 
-    select(bs_bez, bs_strasse)
+
