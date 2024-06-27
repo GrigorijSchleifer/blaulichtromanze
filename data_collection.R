a4b5b2b6de892934dda1c7b033e3a6750928fc5f
Grigorij Schleifer <36699154+GrigorijSchleifer@users.noreply.github.com>
2024-06-27 14:11:43 +0200

    read_lines looks promising and stroing text in files as well

--- data_collection.R
+++ data_collection.R
@@ -3,22 +3,31 @@ require(readr)
 data_folder <- "~/github/blaulichtromanze/Krankenhausdaten"
 file_list <- list.files(path = data_folder, pattern = "*.csv") 
 
-read_kh_data <- function(file_location) {
+read_kh_data <- function(files) {
     # read in each .csv file in file_list
     # create a data frame with the same name as the .csv file
     # Args:
         # character vector with locating the files
     # Return:
         # not sure yet
-    for (i in 1:length(file_list)) {
-        assign(file_list[i],
-               read_delim(file = paste(data_folder, file_list[i], sep = "/"),
-                          delim = ";",
-                          escape_double = FALSE, 
-                          trim_ws = TRUE,
-                          locale = locale(encoding = "ISO-8859-1"))
-        )
-    }
+    for (i in 1:length(files)) {
+        gsub(files[i], ".csv", "") <- read_delim(file = paste(data_folder, files[i], sep = "/"),
+                                         delim = ";",
+                                         escape_double = FALSE, 
+                                         trim_ws = TRUE,
+                                         show_col_types = FALSE,
+                                         locale = locale(encoding = "ISO-8859-1"))
+                 }
 }
 
+read_kh_data(file_list)
 
+kh <- read_delim(file = paste(data_folder, file_list[1], sep = "/"),
+           delim = ";",
+           escape_double = FALSE, 
+           trim_ws = TRUE,
+           show_col_types = FALSE,
+           locale = locale(encoding = "ISO-8859-1"))
+
+txt <- read_lines("inserate/ 03_09_2023_08_00.txt")
+txt
